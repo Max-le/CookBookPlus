903f73775b56838802455a598b44ff147a1f8ba0
Max <max.09.outlook.com>
2020-04-12 12:30:26 +0200

    adding constraints

--- src/main/java/com/maxle/cookBookPlus/models/DTO/user/UserCreationDTO.java
+++ src/main/java/com/maxle/cookBookPlus/models/DTO/user/UserCreationDTO.java
@@ -1,4 +1,20 @@
 package com.maxle.cookBookPlus.models.DTO.user;
 
+import lombok.Setter;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+// https://www.baeldung.com/registration-with-spring-mvc-and-spring-security#the-user-dto-object@Getter @Setter
 public class UserCreationDTO {
+
+    @NotNull @NotBlank
+    private String username;
+
+    @NotNull @NotBlank
+    private String email;
+
+    @NotNull @NotBlank
+    private String password;
 }
