e3e86d5370ff6a9bac943927b2d28d16a8c61b85
Max <max.09@outlook.com>
2020-04-13 15:24:03 +0200

    find User by ID

--- src/main/java/com/maxle/cookBookPlus/controllers/UserController.java
+++ src/main/java/com/maxle/cookBookPlus/controllers/UserController.java
@@ -1,17 +1,20 @@
 package com.maxle.cookBookPlus.controllers;
 
 
+import com.maxle.cookBookPlus.models.DTO.user.UserCreationDTO;
+import com.maxle.cookBookPlus.models.DTO.user.UserDTO;
 import com.maxle.cookBookPlus.models.entities.User;
 import com.maxle.cookBookPlus.service.user.UserService;
 import com.maxle.cookBookPlus.service.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("/greeting")
@@ -26,16 +29,30 @@ public class UserController {
         this.userService = userServ;
     }
 
-    @GetMapping("/all")
+    @GetMapping(value = {"/", ""})
     public List<User> getAll() {
-        List<User> users = userService.findAll();
-        return users;
+        return userService.findAll();
     }
 
     @PostMapping(value = {"/", ""})
     public String create(){
-        return "This is the endpoint to create a user. Send a POST request here !";
+        return "You've reached the endpoint to create a user!";
 
+    }
+
+    @GetMapping(value = {"{id}"})
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Long id) {
+        User u = userService.findById(id);
+        if (u == null) {return null;}
+        UserDTO uDTO = new UserDTO();
+
+        //Mapping User => UserDTO
+        uDTO.setId(u.getId());
+        uDTO.setEmail(u.getEmail());
+        uDTO.setUsername(u.getUsername());
+        //
+
+        return ResponseEntity.ok(uDTO);
     }
 
 }
