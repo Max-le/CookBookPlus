576251515d1af7db0140e4c5fd3942d7ea4f2b68
Max <max.09@outlook.com>
2020-04-14 16:29:04 +0200

    JPA for Recipes and user-recipe rel

--- src/main/java/com/maxle/cookBookPlus/service/user/UserServiceImpl.java
+++ src/main/java/com/maxle/cookBookPlus/service/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.maxle.cookBookPlus.service.user;
 
-import com.maxle.cookBookPlus.models.entities.Recipe;
+import com.maxle.cookBookPlus.mappers.RecipeMapper;
+import com.maxle.cookBookPlus.models.DTO.user.RecipeDTO;
 import com.maxle.cookBookPlus.models.entities.User;
 import com.maxle.cookBookPlus.repositories.UserRepository;
 import lombok.NonNull;
@@ -9,15 +10,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepo;
+    private final RecipeMapper recipeMapper;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepo){
+    public UserServiceImpl(UserRepository userRepo, RecipeMapper mapper){
         this.userRepo = userRepo;
+        this.recipeMapper = mapper;
     }
 
     @Override
@@ -36,9 +40,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<Recipe> findRecipesOfUser(@NonNull Long chef_user_id) {
-//        Optional<User> userOptional = this.userRepo.findById(chef_user_id);
-//        if (!userOptional.isPresent()) return null;
-        return null;
+    public List<RecipeDTO> findRecipesOfUser(@NonNull Long chef_user_id) {
+        Optional<User> userOptional = this.userRepo.findById(chef_user_id);
+        if (!userOptional.isPresent()) return null;
+        User user = userOptional.get();
+
+        List<RecipeDTO> recipesDTO = user.getRecipes().stream().map( r -> {
+
+                 return recipeMapper.toRecipeDTO(r) ;
+
+        }).collect(Collectors.toList());
+
+        return recipesDTO;
+
     }
 }
