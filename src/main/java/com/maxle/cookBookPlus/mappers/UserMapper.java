30b7fdac9af2a7a690eeac270328c7e81540ba4c
Max <max.09@outlook.com>
2020-04-15 09:55:13 +0200

    new mapper for user

---
+++ src/main/java/com/maxle/cookBookPlus/mappers/UserMapper.java
@@ -0,0 +1,21 @@
+package com.maxle.cookBookPlus.mappers;
+
+
+import com.maxle.cookBookPlus.models.DTO.user.UserInfoDTO;
+import com.maxle.cookBookPlus.models.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserInfoDTO toUserInfoDTO(User u){
+
+        UserInfoDTO uDTO = new UserInfoDTO();
+
+        uDTO.setEmail(u.getEmail());
+        uDTO.setUsername(u.getUsername());
+        uDTO.setId(u.getId());
+
+        return uDTO;
+    }
+}
