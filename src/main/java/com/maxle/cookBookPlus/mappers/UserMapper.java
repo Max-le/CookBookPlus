7dd28da4d8668355407da7aa52516b82e74bf018
Max <max.09@outlook.xom>
2020-04-29 14:46:47 +0200

    add function user mapper

--- src/main/java/com/maxle/cookBookPlus/mappers/UserMapper.java
+++ src/main/java/com/maxle/cookBookPlus/mappers/UserMapper.java
@@ -1,20 +1,29 @@
 package com.maxle.cookBookPlus.mappers;
 
 
+import com.maxle.cookBookPlus.models.DTO.user.UserCreationDTO;
 import com.maxle.cookBookPlus.models.DTO.user.UserInfoDTO;
-import com.maxle.cookBookPlus.models.entities.chefUser;
+import com.maxle.cookBookPlus.models.entities.ChefUser;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
-    public UserInfoDTO toUserInfoDTO(chefUser u){
+    public UserInfoDTO toUserInfoDTO(ChefUser u){
 
         UserInfoDTO uDTO = new UserInfoDTO();
-
         uDTO.setUsername(u.getUsername());
         uDTO.setId(u.getId());
 
         return uDTO;
+    }
+
+    public ChefUser toUser(UserCreationDTO uDTO){
+
+        ChefUser user = new ChefUser();
+        user.setUsername(uDTO.getEmail());
+        user.setPassword(uDTO.getPassword());
+
+        return user;
     }
 }
