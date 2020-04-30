3bae4028eeb6507afa1319fce8f4f41d489c6a5b
Max <max.09@outlook.xom>
2020-04-30 10:53:35 +0200

    proper call to get web resources

--- src/main/java/com/maxle/cookBookPlus/controllers/UserController.java
+++ src/main/java/com/maxle/cookBookPlus/controllers/UserController.java
@@ -79,7 +79,7 @@ public class UserController {
     //Get all web resources (bookmarks) of a one user
     @GetMapping(value = "{id}/bookmarks")
     public ResponseEntity<List<WebResourceDTO>> getBookmarksOfOneUser(@PathVariable("id") Long id){
-        List<WebResourceDTO> bookmarks = webResService.find();
+        List<WebResourceDTO> bookmarks = userService.findResourcesOfUser(id);
         return ResponseEntity.ok(bookmarks);
     }
 
